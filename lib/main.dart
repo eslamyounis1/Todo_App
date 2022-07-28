e930868e253449c5c8b022f6c76839322c581cba
eslamyounis1 <eslamupwork@gmail.com>
2022-07-28 02:56:04 +0200

    change_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled3/shared/bloc_observer.dart';
 
 import 'layout/home_layout.dart';
 
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+    () {
+      runApp(const MyApp());
+    },
+    blocObserver: MyBlocObserver(),
+  );
 }
 
-class MyApp  extends StatelessWidget {
-  const MyApp ({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
